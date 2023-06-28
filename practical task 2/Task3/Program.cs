c1462fa93b97a24733674e9df2806f51cb7b3c4e
RudStepan <1gooder2@gmail.com>
2023-06-28 14:05:18 +0700

    Added practical task 2

---
+++ practical task 2/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 0 || number > 7)
+{
+    Console.WriteLine("Введено неверное число");
+}
+else
+{
+    if (number > 5)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
